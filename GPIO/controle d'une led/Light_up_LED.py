42dc9686042d074074581fb952575952a8a244d9
didier <didier.roth@student.hepl.be>
2024-02-12 09:10:26 +0100

    :sparkles: add resources branch

--- GPIO/controle d'une led/Light_up_LED.py
+++
@@ -1,3 +0,0 @@
-from machine import Pin
-LED = machine.Pin(16,machine.Pin.OUT)
-LED.value(1)
--- GPIO/controle d'une led/blink_While-Loop.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin
-from utime import sleep
-LED = machine.Pin(16,machine.Pin.OUT)
-
-while True:
-    LED.value(True)
-    sleep(1)
-    LED.value(False)
-    sleep(1)
